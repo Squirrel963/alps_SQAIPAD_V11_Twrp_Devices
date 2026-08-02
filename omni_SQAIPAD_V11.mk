a9dd98cae9da2226fd7cb011da2049295950a0d4
Squirrel963 <111523440+Squirrel963@users.noreply.github.com>
2026-08-02 13:23:27 +0800

    Update omni_SQAIPAD_V11.mk

--- omni_SQAIPAD_V11.mk
+++ omni_SQAIPAD_V11.mk
@@ -10,10 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from SQAIPAD-V11 device
-$(call inherit-product, device/alps/SQAIPAD-V11/device.mk)
+# $(call inherit-product, device/alps/SQAIPAD-V11/device.mk)
 
 PRODUCT_DEVICE := SQAIPAD-V11
 PRODUCT_NAME := omni_SQAIPAD-V11
